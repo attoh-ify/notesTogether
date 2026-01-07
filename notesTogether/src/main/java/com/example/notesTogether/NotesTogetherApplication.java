8e416ea907049cc62ca66771daeafca1c58cd61c
Attoh Ifeanyi <alexander.attoh22@gmail.com>
2026-01-07 11:17:49 +0100

    feat: Initial project setup

---
+++ notesTogether/src/main/java/com/example/notesTogether/NotesTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesTogetherApplication.class, args);
+	}
+
+}
---
+++ notesTogether/src/test/java/com/example/notesTogether/NotesTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
